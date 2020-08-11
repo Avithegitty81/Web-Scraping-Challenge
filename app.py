e2d36d4c9034c17a4b122f781438572fc7a3f4dc
Avithegitty81 <64101146+Avithegitty81@users.noreply.github.com>
2020-08-10 23:27:15 -0500

    HW. revised

---
+++ app.py
@@ -0,0 +1,47 @@
+form flask import Flask, render_template, redirect
+import pymongo
+import scrape_mars
+
+
+# Create Flask
+app = Flask(__name__)
+
+
+# setup mongo connection
+conn = "mongodb://localhost:27017"
+client = pymongo.MongoClient(conn)
+
+
+# connect to mongo db and connection
+db = client.mars_db
+mars = db.mars_collection
+mars.insert_one(scrape_mars.scrape())
+
+@app.route("/")
+def home():
+    mars_data = mars.find_one()
+    return render_template("index.html", mars_data = mars_data)
+
+@app.route("/scrape")
+def scrape():
+    
+    
+    # Run the scrape function
+    data = scrape_mars.scrape()
+    
+    
+    #update the Mongo database using update and upsert and upsert=True
+    mars.update(
+        {},
+        data,
+        upsert=True
+        
+    )
+        
+    # Redirect back to home page
+    return redirect("/")
+
+if __name__ == "__main__":
+    app.run(debug = False)
+
+    
